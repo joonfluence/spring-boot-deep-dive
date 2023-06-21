e315c788a3d950d5c7845439bb0d9a69a8392b07
joonho lee <joonfluence.dev@gmail.com>
2023-06-21 17:11:03 +0900

    [FEAT] Post 생성

---
+++ src/main/java/com/joonfluence/springboot/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.joonfluence.springboot.service.posts;
+
+import com.joonfluence.springboot.domain.posts.PostsRepository;
+import com.joonfluence.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
---
+++ src/test/java/com/joonfluence/springboot/web/HelloControllerTest2.java
@@ -0,0 +1,7 @@
+package com.joonfluence.springboot.web;
+
+import static org.junit.Assert.*;
+
+public class HelloControllerTest2 {
+
+}
