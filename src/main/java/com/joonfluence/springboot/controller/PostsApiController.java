8406613e269544629035f1e91797904afb5da2ca
joonho lee <joonfluence.dev@gmail.com>
2023-06-22 15:57:31 +0900

    [FEAT] 게시글 CRUD

--- src/main/java/com/joonfluence/springboot/controller/PostsApiController.java
+++ src/main/java/com/joonfluence/springboot/controller/PostsApiController.java
@@ -1,10 +1,10 @@
 package com.joonfluence.springboot.controller;
 
 import com.joonfluence.springboot.service.posts.PostsService;
+import com.joonfluence.springboot.web.dto.PostsResponseDto;
+import com.joonfluence.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -15,5 +15,21 @@ public class PostsApiController {
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody com.joonfluence.springboot.web.dto.PostsSaveRequestDto requestDto){
         return postsService.save(requestDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id, requestDto);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id){
+        postsService.delete(id);
+        return id;
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id){
+        return postsService.findById(id);
     }
 }
--- src/main/java/com/joonfluence/springboot/service/posts/PostsService.java
+++ src/main/java/com/joonfluence/springboot/service/posts/PostsService.java
@@ -1,11 +1,17 @@
 package com.joonfluence.springboot.service.posts;
 
+import com.joonfluence.springboot.domain.posts.Posts;
 import com.joonfluence.springboot.domain.posts.PostsRepository;
+import com.joonfluence.springboot.web.dto.PostsListResponseDto;
+import com.joonfluence.springboot.web.dto.PostsResponseDto;
 import com.joonfluence.springboot.web.dto.PostsSaveRequestDto;
+import com.joonfluence.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -15,5 +21,28 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    @Transactional
+    public void delete(Long id){
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        postsRepository.delete(posts);
+    }
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        return new PostsResponseDto(entity);
+    }
+
+    @Transactional
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
     }
 }
