8542a59542216d0a78f03841d7ac7abfbc2114af
joonho lee <joonfluence.dev@gmail.com>
2023-06-23 22:25:15 +0900

    [FEAT] 네이버 로그인 및 인증 포함 테스트 케이스 작성

--- src/main/java/com/joonfluence/springboot/controller/IndexController.java
+++ src/main/java/com/joonfluence/springboot/controller/IndexController.java
@@ -1,5 +1,6 @@
 package com.joonfluence.springboot.controller;
 
+import com.joonfluence.springboot.config.auth.LoginUser;
 import com.joonfluence.springboot.config.auth.dto.SessionUser;
 import com.joonfluence.springboot.service.posts.PostsService;
 import com.joonfluence.springboot.web.dto.PostsResponseDto;
@@ -20,9 +21,9 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
         if(user != null){
             model.addAttribute("userName", user.getName());
         }
